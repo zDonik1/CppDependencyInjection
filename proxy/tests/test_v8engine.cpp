29aaed1a1a1b520fd760f58d46d078d205c90317
Doniyor Tokhirov <tokhirovdoniyor@gmail.com>
2022-09-16 10:07:15 +0500

    fix: changed v8engine tests to use proxy

--- proxy/tests/test_v8engine.cpp
+++ proxy/tests/test_v8engine.cpp
@@ -1,43 +1,79 @@
 #include <gmock/gmock.h>
+#include <proxy/proxy.h>
 
 #include <v8engine.h>
 
 using namespace std;
 using namespace testing;
+using namespace pro;
+
+
+struct Start : pro::dispatch<bool()>
+{
+    template<class Self>
+    inline auto operator()(Self &self)
+    {
+        return self.start();
+    }
+};
+
+struct IsRunning : pro::dispatch<bool()>
+{
+    template<class Self>
+    inline auto operator()(Self &self)
+    {
+        return self.isRunning();
+    }
+};
+
+struct Stop : pro::dispatch<void()>
+{
+    template<class Self>
+    inline auto operator()(Self &self)
+    {
+        self.stop();
+    }
+};
+
+struct FEngine : pro::facade<Start, IsRunning, Stop>
+{};
 
 
 class TestV8Engine : public Test
 {
 public:
-    V8Engine engine;
+    inline auto start() { return engine.invoke<Start>(); }
+    inline auto isRunning() { return engine.invoke<IsRunning>(); }
+    inline auto stop() { return engine.invoke<Stop>(); }
+
+public:
+    proxy<FEngine> engine{make_proxy<FEngine, V8Engine>()};
 };
 
 
 TEST_F(TestV8Engine, StartSucceeds)
 {
-    ASSERT_THAT(engine.start(), IsTrue());
+    ASSERT_THAT(start(), IsTrue());
 }
 
-TEST(TestV8ConstEngine, NotRunningByDefault)
+TEST_F(TestV8Engine, NotRunningByDefault)
 {
-    const V8Engine engine;
-
-    ASSERT_THAT(engine.isRunning(), IsFalse());
+    ASSERT_THAT(isRunning(), IsFalse());
 }
 
 TEST_F(TestV8Engine, IsRunningAfterStarting)
 {
-    engine.start();
+    start();
 
-    ASSERT_THAT(engine.isRunning(), IsTrue());
+    ASSERT_THAT(isRunning(), IsTrue());
 }
 
 TEST_F(TestV8Engine, NotRunningAfterStopping)
 {
-    engine.start();
-    ASSERT_THAT(engine.isRunning(), IsTrue());
+    start();
+    ASSERT_THAT(isRunning(), IsTrue());
 
-    engine.stop();
+    stop();
 
-    ASSERT_THAT(engine.isRunning(), IsFalse());
+    ASSERT_THAT(isRunning(), IsFalse());
 }
